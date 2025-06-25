1b010a73c41ad741dbab792d46af9195e2c34573
Phung Gia Minh <vestonnamdinh@gmail.com>
2025-06-25 22:06:30 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,23 +1,59 @@
 #include <bits/stdc++.h>
-#define name vestonluvto
+#define name "vestonluvto"
+#define namein name".inp"
+#define nameout name".out"
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
 using namespace std;
 
-void home(){
-     if (fopen("name.inp", "r")) {
-        freopen("name.inp", "r", stdin);
-        freopen("name.out", "w", stdout);
-    }
+#ifndef vestonluvto
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(int t) {cerr << t;}
+void _print(long long t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(long double t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(unsigned long long t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+void homefix(){
+    #ifndef vestonluvto
+	freopen("fixcode.txt", "w", stderr);
+    #endif
 }
 
-void nhap(){}
+void home(){
+     if (fopen(namein, "r")) {
+        freopen(namein, "r", stdin);
+        freopen(nameout, "w", stdout);
+     }
+}
+
+void nhap(){
+}
 
 void bfsolve(){}
 
-void solve(){bfsolve();}
+void solve(){bfsolve();
+}
 
 int main(){
-    ios_base::sync_with_stdio(0); home();
-    cin.tie(0); cout.tie(0); int t = 1;
+    fastio(); home();int t = 1;
+    //homefix();
     //cin >> t;
     while(t--)nhap(), solve();
     return 0;
